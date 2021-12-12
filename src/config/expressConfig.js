08c457e710b941443cc8d1f4202425dc754e8b95
reeidi <reeidi@abv.bg>
2021-12-12 04:27:49 +0200

    Enable CORS

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const cors = require('cors');
 const express = require('express');
 const cookieParser = require('cookie-parser')
 
 function configure(app) {
+    app.use(cors());
     app.use('/static', express.static(path.resolve(__dirname, '../public')));  // Redirect all lookup for resources for '/static' to '../public'.
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
